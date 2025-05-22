9a0e5881d8a41ba3beda4229168e2dd815b8a67e
nabin0707 <naveenghimire07@gmail.com>
2025-05-22 21:09:16 +0545

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -4,17 +4,39 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool isDarkMode = false;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Simple Flutter App',
-      home: MyHomePage(),
+      title: 'Updated Counter App',
+      theme: isDarkMode ? ThemeData.dark() : ThemeData.light(),
+      home: MyHomePage(
+        isDarkMode: isDarkMode,
+        toggleTheme: () {
+          setState(() {
+            isDarkMode = !isDarkMode;
+          });
+        },
+      ),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
+  final bool isDarkMode;
+  final VoidCallback toggleTheme;
+
+  const MyHomePage({super.key, required this.isDarkMode, required this.toggleTheme});
+
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
@@ -22,29 +44,46 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++; // Increases counter by 1
-    });
-  }
+  void _incrementCounter() => setState(() => _counter++);
+  void _resetCounter() => setState(() => _counter = 0);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Counter App'),
+        actions: [
+          IconButton(
+            icon: Icon(widget.isDarkMode ? Icons.light_mode : Icons.dark_mode),
+            onPressed: widget.toggleTheme,
+          ),
+        ],
       ),
       body: Center(
-        child: Text(
-          'Counter: $_counter',
-          style: TextStyle(fontSize: 24),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('You have pushed the button this many times:'),
+            Text(
+              '$_counter',
+              style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton.icon(
+              icon: Icon(Icons.refresh),
+              label: Text('Reset'),
+              onPressed: _resetCounter,
+            ),
+          ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
-        child: Icon(Icons.add),
         tooltip: 'Increment',
+        child: Icon(Icons.add),
       ),
     );
   }
 }
+// This code is a simple Flutter app that implements a counter with dark mode support.
+// The app has a main screen with a counter, a reset button, and a toggle for dark mode.
